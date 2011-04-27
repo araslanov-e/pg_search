062f2cdc290cd6b86d874bc2684b52f952511b74
Scott Lowe <scottwithtwotees@gmail.com>
2011-04-27 13:10:55 +0100

    Remove use of 'class << self' in attempt to save horizontal blog space

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,27 +1,25 @@
 class Article < ActiveRecord::Base
   validates :title, :body, :presence => true
 
-  class << self
-    # Note that ActiveRecord ARel from() doesn't appear to accommodate "?"
-    # param placeholder, hence the need for manual parameter sanitization
-    def tsearch_query(search_terms, limit = 25)
-      words = sanitize(search_terms.scan(/\w+/) * "|")
+  # Note that ActiveRecord ARel from() doesn't appear to accommodate "?"
+  # param placeholder, hence the need for manual parameter sanitization
+  def self.tsearch_query(search_terms, limit = 25)
+    words = sanitize(search_terms.scan(/\w+/) * "|")
 
-      Article.from("articles, to_tsquery('pg_catalog.english', #{words}) as q").
-        where("tsv @@ q").order("ts_rank_cd(tsv, q) DESC").limit(limit)
-    end
+    Article.from("articles, to_tsquery('pg_catalog.english', #{words}) as q").
+      where("tsv @@ q").order("ts_rank_cd(tsv, q) DESC").limit(limit)
+  end
 
-    # Selects search results with plain text title & body columns.
-    # Select columns are explicitly listed to avoid returning the long redundant tsv strings
-    def plain_tsearch(search_terms, limit)
-      select([:title, :body, :id]).tsearch_query(search_terms, limit)
-    end
+  # Selects search results with plain text title & body columns.
+  # Select columns are explicitly listed to avoid returning the long redundant tsv strings
+  def self.plain_tsearch(search_terms, limit)
+    select([:title, :body, :id]).tsearch_query(search_terms, limit)
+  end
 
-    # Select search results with HTML highlighted title & body columns
-    def highlight_tsearch(search_terms, limit)
-      body = "ts_headline(body, q, 'StartSel=<strong>, StopSel=</strong>, HighlightAll=TRUE') as body"
-      title = "ts_headline(title, q, 'StartSel=<strong>, StopSel=</strong>, HighlightAll=TRUE') as title"
-      Article.select([body, title, :id]).tsearch_query(search_terms, limit)
-    end
+  # Select search results with HTML highlighted title & body columns
+  def self.highlight_tsearch(search_terms, limit)
+    body = "ts_headline(body, q, 'StartSel=<strong>, StopSel=</strong>, HighlightAll=TRUE') as body"
+    title = "ts_headline(title, q, 'StartSel=<strong>, StopSel=</strong>, HighlightAll=TRUE') as title"
+    Article.select([body, title, :id]).tsearch_query(search_terms, limit)
   end
 end
